d49475a1e484d7bf04772338497b7d1ec19b68d6
Robooto <samrussell63@gmail.com>
2016-01-05 20:37:34 -0800

    Updating and fixes

--- main.js
+++ main.js
@@ -1,57 +1,108 @@
 'use strict';
 
-var textCounter = {
-    init: function(options) {
-        this.defaults = {
-            width: 300,
-            side: "left"
+// factory create textCounter w/Options
+function CreateTextCounter(opts) {
+    var createCounter = {
+        init: function (opts) {
+            textCounter.call(this).bootStrap(opts);
+            createCounter.prototype = Object.create(textCounter);
+
+            return this;
         }
-        this._options = $.extend( {}, this.defaults, options );
-        this.$el = $('#' + options.elem).css("width", "100%");
-        this.addContainer();
-        this.addEvents();
-        
+    }
+    if (!opts.hasOwnProperty("elem")) {
+        throw new Error("elem must be defined");
+    }
+    //$.extend(createCounter, apiCall);
+    return Object.create(createCounter).init(opts);
+}
+
+function textCounter() {
+
+    // Private Vars
+    var _ = {
+        width: 300,
+        side: "left",
+        $el: null,
+        count: 100
+    };
+
+    // public functions
+    this.bootStrap = function (options) {
+        this.init(options)
+            .addContainer()
+            .addEvents()
+    };
+    
+    this.init = function (options) {
+        _ = $.extend({}, _, options);
+        _.$el = $('#' + options.elem).css("width", "100%");
+
         return this;
-    },
-    addEvents: function () {
-        var self = this;
-        this.$el.on('keyup', function (e) {
-            if ($(this).val().length > self.count){
-                $(this).val($(this).val().substr(0, self.count));
-            }
-            $(this).siblings('span').children('span').text(self.countLeft);
-           
-        });  
-    },
-    get count() {
-        return this._options.count;
-    },
-    get textLength() {
-        return Number(this.$el.val().length);
-    },
-    get countLeft() {
-        return this.count - this.textLength;  
-    },
-    addContainer: function() {
-        var div =  $('<div>', { class: 'areaContainer', style: 'width: ' + 300 + 'px;',});
-        this.$el.wrap(div);
-        this.$el.parent('div')
-            .append($('<span>', 
-                {  
-                    class: 'areaCounter', 
-                    text: ' Characters remain out of ' + this.count, 
+    };
+    
+    this.addContainer = function () {
+        var div = $('<div>', {
+            class: 'areaContainer',
+            style: 'width: ' + _.width + 'px;',
+        });
+        _.$el.wrap(div);
+        _.$el.parent('div')
+            .append($('<span>', {
+                    class: 'areaCounter',
+                    text: ' Characters remain out of ' + _.count,
                     style: 'display: block'
                 })
-            .prepend($('<span>', {id: 'remain', text: this.countLeft})));
-    },
-    validate: function() {
-        this.$el.removeClass("ui-state-error");
+                .prepend($('<span>', {
+                    id: 'remain',
+                    text: this.countLeft()
+                }))
+            );
+        return this;
+    };
+
+    this.addEvents = function () {
+        var self = this;
+        _.$el.on('keyup', function (e) {
+            if ($(this).val().length > self.count()) {
+                $(this).val($(this).val().substr(0, self.count()));
+            }
+            $(this).siblings('span').children('span').text(self.countLeft());
+
+        }); 
         
-        if(this.$el.val() > this.count) {
-            this.$el.addClass("ui-state-error");
+        return this;
+    };
+
+    this.count = function () {
+        return _.count;
+    };
+
+    this.textLength = function () {
+        return Number(_.$el.val().length);
+    };
+    this.countLeft = function () {
+        return _.count - this.textLength();
+    };
+
+    this.validate = function () {
+        _.$el.removeClass("ui-state-error");
+
+        if (_.$el.val() > _.count) {
+            _.$el.addClass("ui-state-error");
             return false;
         }
-        
         return true;
     }
+
+
+    return this;
 }
+
+
+var apiCall = {
+    getAPI: function (url, callback, data) {
+        console.log("call to " + url + " successful");
+        callback("you did it!");
+    }
+};
--- tests/spec/TextCounter.js
+++ tests/spec/TextCounter.js
@@ -12,28 +12,71 @@ describe("textCounter", function () {
     
     beforeEach(function () {
         $('#fixture')
-            .append($('<textarea>', 
+            .html($('<textarea>', 
                 {
-                    id: "area"
+                    id: "area",
+                    val: "test"
                 }
             )
         );
-        textCounter = CreateTextCounter({
+        
+        textArea = CreateTextCounter({
             elem: "area",
             count: 10
         });
+        
+
+    });
+        
+    afterEach(function () {
+        $('#fixture').hide();
     });
     
-    afterEach(function () {
-        $('#fixture').remove();
+    describe("Initalization tests", function () {
+        
+        it('Should have added dom elements', function() {
+            expect($('#remain').length).toEqual(1);
+            expect($('.areaCounter').length).toEqual(1);
+        });
+        
+        it('Should have a starting text of test', function() {
+            expect($("#area").val()).toEqual("test");
+        });
     });
     
     describe("text counter logic", function () {
-    
-        it('should have a max count of 10', function() {
-            expect(textCounter.count).toEqual(10);
+        
+        it('Should have a max count of 10', function() {
+            expect(textArea.count()).toEqual(10);
+        });
+        
+        it('Should have a count left of 6', function() {
+            expect(textArea.countLeft()).toEqual(6);
         });
     
-    })
+    });
+    
+    describe('Event Testing', function() {
+        beforeEach(function() {
+            $('#area').val("1234567890111");
+            $('#area').trigger('keyup');
+        });
+
+        it('Should limit characters to 10', function() {             
+            expect($("#area").val()).toEqual("1234567890");
+        });       
+
+    });
+
+    describe('Validation Testing', function() {
+        beforeEach(function() {
+            $('#area').val('123123131313131313');
+        });          
+
+        it('should fail validation when more than 10 characters are in the field', function() {           
+            expect(textArea.validate()).toBeFalsy();
+        });      
+
+    });
 
 });
